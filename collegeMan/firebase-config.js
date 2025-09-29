855e212f4d9013305a6d7cbc347ca33c84887b52
Arirama Selvam M <ariofficial9787@gmail.com>
2025-09-29 14:41:44 +0530

    first demo

---
+++ collegeMan/firebase-config.js
@@ -0,0 +1,17 @@
+const firebaseConfig = {
+
+  apiKey: "AIzaSyA5dtwCn09lEtrAOP3ih_Iq9YDFSKB181w",
+
+  authDomain: "birthdaycountdown9787.firebaseapp.com",
+
+  projectId: "birthdaycountdown9787",
+
+  storageBucket: "birthdaycountdown9787.firebasestorage.app",
+
+  messagingSenderId: "1057605732413",
+
+  appId: "1:1057605732413:web:18768b4a6bb5a7c2f64254",
+
+  measurementId: "G-M3914S92YP"
+
+};
